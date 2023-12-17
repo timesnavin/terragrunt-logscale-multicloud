6547529063673cfa590e086fa8b10640a26a7cec
Ryan Faircloth <ryan@dss-i.com>
2023-12-17 17:40:59 -0500

    Update main.tf

--- _modules/aws/kubernetes/module/main.tf
+++ _modules/aws/kubernetes/module/main.tf
@@ -75,11 +75,14 @@ module "eks" {
       before_compute           = true
       service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
       configuration_values = jsonencode({
-        env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
+        enableCloudWatchLogs = "true"
+        healthProbeBindAddr  = "8163"
+        metricsBindAddr      = "8162"
+        # env = {
+        #   # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+        #   ENABLE_PREFIX_DELEGATION = "true"
+        #   WARM_PREFIX_TARGET       = "1"
+        # }
       })
     }
     coredns = {
