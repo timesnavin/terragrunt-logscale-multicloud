deda322988d09df03999de098991ad809717fb36
Ryan Faircloth <ryan@dss-i.com>
2023-12-18 13:26:15 -0500

    Update main.tf

--- _modules/aws/kubernetes/module/main.tf
+++ _modules/aws/kubernetes/module/main.tf
@@ -1,14 +1,14 @@
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     # This requires the awscli to be installed locally where Terraform is executed
-#     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-#   }
-# }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
 
 # provider "helm" {
 #   kubernetes {
@@ -135,7 +135,7 @@ module "eks" {
 
   enable_irsa = true
   # create_aws_auth_configmap = true
-  # manage_aws_auth_configmap = true
+  manage_aws_auth_configmap = true
   # manage_aws_auth 
   aws_auth_roles = concat(
     # We need to add in the Karpenter node IAM role for nodes launched by Karpenter
