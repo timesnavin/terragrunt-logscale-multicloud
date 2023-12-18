0022ad14ddd96f953ef50cf433e5fa23e25aada2
Ryan Faircloth <ryan@dss-i.com>
2023-12-18 18:59:42 -0500

    Update karpenter.tf

--- _modules/aws/kubernetes/module/karpenter.tf
+++ _modules/aws/kubernetes/module/karpenter.tf
@@ -15,3 +15,155 @@ module "karpenter" {
   }
 
 }
+
+
+################################################################################
+# Karpenter
+################################################################################
+
+
+
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name                = "karpenter"
+  repository          = "oci://public.ecr.aws/karpenter"
+  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart               = "karpenter"
+  version             = "v0.33.0"
+
+  values = [
+    <<-EOT
+    settings:
+      clusterName: ${module.eks.cluster_name}
+      clusterEndpoint: ${module.eks.cluster_endpoint}
+      interruptionQueueName: ${module.karpenter.queue_name}
+    serviceAccount:
+      annotations:
+        eks.amazonaws.com/role-arn: ${module.karpenter.irsa_arn} 
+    EOT
+  ]
+
+}
+
+resource "kubectl_manifest" "karpenter_node_class" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.k8s.aws/v1beta1
+    kind: EC2NodeClass
+    metadata:
+      name: default
+    spec:
+      amiFamily: AL2
+      role: ${module.karpenter.role_name}
+      subnetSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks.cluster_name}
+      securityGroupSelectorTerms:
+        - tags:
+            karpenter.sh/discovery: ${module.eks.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${module.eks.cluster_name}
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+resource "kubectl_manifest" "karpenter_default_arm_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default-arm64
+    spec:
+      template:
+        metadata:
+          # Labels are arbitrary key-values that are applied to all nodes
+          labels:
+            computeType: "compute"
+            storageType: "network"
+            durabilityType: "spot"
+        spec:
+          nodeClassRef:
+            name: default
+          requirements:
+            - key: karpenter.sh/capacity-type
+              operator: In
+              values: ["spot"]          
+            - key: "karpenter.k8s.aws/instance-category"
+              operator: In
+              values: ["c", "m", "r"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["4", "8", "16", "32"]
+            - key: "karpenter.k8s.aws/instance-hypervisor"
+              operator: In
+              values: ["nitro"]
+            - key: "kubernetes.io/arch"
+              operator: In
+              values: ["arm64"]              
+            - key: "karpenter.k8s.aws/instance-generation"
+              operator: Gt
+              values: ["2"]
+      limits:
+        cpu: 1000
+      disruption:
+        consolidationPolicy: WhenUnderutilized
+  YAML
+
+  depends_on = [
+    kubectl_manifest.karpenter_node_class
+  ]
+}
+
+
+resource "kubectl_manifest" "karpenter_default_intel_node_pool" {
+  yaml_body = <<-YAML
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default-amd64
+    spec:
+      template:
+        metadata:
+          # Labels are arbitrary key-values that are applied to all nodes
+          labels:
+            computeType: "compute"
+            storageType: "network"
+            durabilityType: "spot"
+        spec:
+          nodeClassRef:
+            name: default
+          requirements:
+            - key: karpenter.sh/capacity-type
+              operator: In
+              values: ["spot"]          
+            - key: "karpenter.k8s.aws/instance-category"
+              operator: In
+              values: ["c", "m", "r"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["4", "8", "16", "32"]
+            - key: "karpenter.k8s.aws/instance-hypervisor"
+              operator: In
+              values: ["nitro"]
+            - key: "kubernetes.io/arch"
+              operator: In
+              values: ["amd64"]              
+            - key: "karpenter.k8s.aws/instance-generation"
+              operator: Gt
+              values: ["2"]
+      limits:
+        cpu: 1000
+      disruption:
+        consolidationPolicy: WhenUnderutilized
+  YAML
+
+  depends_on = [
+    kubectl_manifest.karpenter_node_class
+  ]
+}
