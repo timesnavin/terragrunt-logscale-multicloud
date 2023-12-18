e75e09069bd98a324d40a7296d95fdd3d31f0534
Ryan Faircloth <ryan@dss-i.com>
2023-12-18 10:00:45 -0500

    reduce

--- _modules/aws/kubernetes/module/main.tf
+++ _modules/aws/kubernetes/module/main.tf
@@ -257,23 +257,23 @@ locals {
 # }
 
 
-module "vpc_cni_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.32.1"
+# module "vpc_cni_irsa" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "5.32.1"
 
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv4   = true
+#   role_name_prefix      = "VPC-CNI-IRSA"
+#   attach_vpc_cni_policy = true
+#   vpc_cni_enable_ipv4   = true
 
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:aws-node"]
+#     }
+#   }
 
 
-}
+# }
 
 resource "aws_iam_policy" "additional" {
   name = "${var.cluster_name}-additional"
