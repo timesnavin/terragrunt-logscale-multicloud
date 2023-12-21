a9e70c533b38ccc853f52264cdd33fed47e7ad4e
Ryan Faircloth <ryan@dss-i.com>
2023-12-21 16:07:01 -0500

    Create ebs.tf

---
+++ _modules/aws/kubernetes-addons/module/ebs.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_storage_class" "gp2-ext4" {
+  metadata {
+    name = "aws-ebs-gp2-ext4"
+  }
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+  parameters = {
+    fsType = "ext4"
+    type   = "gp2"
+  }
+
+}
+
+resource "kubernetes_storage_class" "gp3-ext4" {
+  metadata {
+    name = "aws-ebs-gp2-ext4"
+  }
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+  parameters = {
+    fsType = "ext4"
+    type   = "gp3"
+  }
+}
