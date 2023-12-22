73088a00a28c6291aae06cc91e4bfde83f5a32a5
Ryan Faircloth <ryan@dss-i.com>
2023-12-22 08:17:32 -0500

    update

--- _modules/aws/route53/zone-partition/module/main.tf
+++ _modules/aws/route53/zone-partition/module/main.tf
@@ -8,3 +8,18 @@ module "zone" {
     }
   }
 }
+
+
+module "zone" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "2.11.0"
+
+  zone_id = var.parent_zone_id
+  records = [{
+    name    = "${var.zone_name}"
+    type    = "NS"
+    ttl     = 600
+    records = module.zone.outputs.route53_zone_name_servers[var.zone_name]
+  }]
+
+}
--- _modules/aws/route53/zone-partition/module/variables.tf
+++ _modules/aws/route53/zone-partition/module/variables.tf
@@ -1,3 +1,8 @@
 variable "zone_name" {
   description = "The name of the zone"
 }
+
+variable "parent_zone_id" {
+  description = "The ID of the parent zone"
+
+}
