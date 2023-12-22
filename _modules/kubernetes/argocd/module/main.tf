693832bc19361ffad22bd3509afda7d2a3f41880
Ryan Faircloth <ryan@dss-i.com>
2023-12-22 16:00:10 -0500

    Update main.tf

--- _modules/kubernetes/argocd/module/main.tf
+++ _modules/kubernetes/argocd/module/main.tf
@@ -25,9 +25,9 @@ server:
     enabled: true
     hosts:
       - argocd.${var.domain_name_region}
-    tls:
-      - hosts:
-        - argocd.${var.domain_name_region}
+    # tls:
+    #   - hosts:
+    #     - argocd.${var.domain_name_region}
     annotations:
       alb.ingress.kubernetes.io/scheme: internet-facing
       link.argocd.argoproj.io/external-link: argocd.${var.domain_name_region}
