c4cd0192cb90a1b5e98abe537745f85be07baf4d
Ryan Faircloth <ryan@dss-i.com>
2023-12-22 17:06:55 -0500

    Create metricsserver.tf

---
+++ _modules/aws/kubernetes-addons/module/metricsserver.tf
@@ -0,0 +1,19 @@
+
+resource "helm_release" "metricsserver" {
+  namespace = "kube-system"
+
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server"
+  # repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+  # repository_password = data.aws_ecrpublic_authorization_token.token.password
+  chart   = "metrics-serve"
+  version = "v3.11.0"
+
+  #   values = [
+  #     <<-EOT
+  #     settings:
+  #     EOT
+  #   ]
+
+
+}
