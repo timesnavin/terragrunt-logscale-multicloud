4f1acea05b5d24f44ec44c6f760b4b3be82dd761
Ryan Faircloth <ryan@dss-i.com>
2023-12-22 19:44:14 -0500

    region cert

---
+++ _modules/aws/acm/cert-region/module/main.tf
@@ -0,0 +1,19 @@
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "5.0.0"
+
+  domain_name = "*.${var.child_domain}.${var.parent_domain}"
+  zone_id     = var.parent_zone_id
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${var.child_domain}.${var.parent_domain}",
+    "${var.child_domain}.${var.parent_domain}",
+  ]
+
+  wait_for_validation = false
+
+  key_algorithm = "EC_secp384r1"
+
+}
