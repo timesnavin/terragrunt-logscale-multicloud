61391c27211036dd6a0b1cf47e7b2d07c28412aa
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 13:21:02 -0500

    switch providers

--- _modules/aws/kubernetes-addons/module/olm.tf
+++ _modules/aws/kubernetes-addons/module/olm.tf
@@ -1,43 +1,43 @@
-# data "kubectl_file_documents" "olm_crds" {
-#     content = file("manifests/olm/crds.yaml")
-# }
-# data "kubectl_file_documents" "olm" {
-#     content = file("manifests/olm/olm.yaml")
-# }
+data "kubectl_file_documents" "olm_crds" {
+    content = file("manifests/olm/crds.yaml")
+}
+data "kubectl_file_documents" "olm" {
+    content = file("manifests/olm/olm.yaml")
+}
 
-# resource "kubernetes_manifest" "olm_crds" {
-#     for_each  = data.kubectl_file_documents.olm_crds.manifests
-#     manifest = yamldecode(each.value)
-# }
+resource "kubernetes_manifest" "olm_crds" {
+    for_each  = data.kubectl_file_documents.olm_crds.manifests
+    manifest = yamldecode(each.value)
+}
 
-# resource "kubernetes_namespace" "olm" {
-#   depends_on = [ time_sleep.olm_wait_destory ]
+resource "kubernetes_namespace" "olm" {
+  depends_on = [ time_sleep.olm_wait_destory ]
 
-#   metadata {
-#     annotations = {
-#       name = "olm"
-#     }
-#     name = "olm"
-#   }
-# }
+  metadata {
+    annotations = {
+      name = "olm"
+    }
+    name = "olm"
+  }
+}
 
-# resource "kubernetes_namespace" "operators" {
-#   depends_on = [ time_sleep.olm_wait_destory ]
+resource "kubernetes_namespace" "operators" {
+  depends_on = [ time_sleep.olm_wait_destory ]
 
-#   metadata {
-#     annotations = {
-#       name = "operators"
-#     }
-#     name = "operators"
-#   }
-# }
+  metadata {
+    annotations = {
+      name = "operators"
+    }
+    name = "operators"
+  }
+}
 
-# resource "kubernetes_manifest" "olm" {
-#     depends_on = [ kubernetes_manifest.olm_crds , kubernetes_namespace.olm, kubernetes_namespace.operators]
-#     for_each  = data.kubectl_file_documents.olm.manifests
-#     manifest = yamldecode(each.value)
-# }
+resource "kubernetes_manifest" "olm" {
+    depends_on = [ kubernetes_manifest.olm_crds , kubernetes_namespace.olm, kubernetes_namespace.operators]
+    for_each  = data.kubectl_file_documents.olm.manifests
+    manifest = yamldecode(each.value)
+}
 
-# resource "time_sleep" "olm_wait_destory" {
-#   destroy_duration = "60s"
-# }
+resource "time_sleep" "olm_wait_destory" {
+  destroy_duration = "60s"
+}
