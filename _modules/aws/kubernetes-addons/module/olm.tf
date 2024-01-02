103cc9095294cb0270edee320e4dd9f50ed497d1
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 12:03:10 -0500

    Update olm.tf

--- _modules/aws/kubernetes-addons/module/olm.tf
+++ _modules/aws/kubernetes-addons/module/olm.tf
@@ -1,8 +1,17 @@
 data "kubectl_file_documents" "olm_crds" {
     content = file("manifests/olm/crds.yaml")
 }
+data "kubectl_file_documents" "olm" {
+    content = file("manifests/olm/olm.yaml")
+}
 
 resource "kubernetes_manifest" "olm_crds" {
     for_each  = data.kubectl_file_documents.olm_crds.manifests
+    manifest = yamldecode(each.value)
+}
+
+resource "kubernetes_manifest" "olm" {
+    depends_on = [ kubernetes_manifest.olm_crds ]
+    for_each  = data.kubectl_file_documents.olm.manifests
     manifest = yamldecode(each.value)
 }
