9d4b8f0ae9fb3d876c73d42e311589451b745551
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 12:13:49 -0500

    updates

--- _modules/aws/kubernetes-addons/module/olm.tf
+++ _modules/aws/kubernetes-addons/module/olm.tf
@@ -10,8 +10,35 @@ resource "kubernetes_manifest" "olm_crds" {
     manifest = yamldecode(each.value)
 }
 
+resource "kubernetes_namespace" "olm" {
+  depends_on = [ time_sleep.olm_wait_destory ]
+
+  metadata {
+    annotations = {
+      name = "olm"
+    }
+
+    name = "olm"
+  }
+}
+resource "kubernetes_namespace" "operators" {
+  depends_on = [ time_sleep.olm_wait_destory ]
+
+  metadata {
+    annotations = {
+      name = "operators"
+    }
+
+    name = "operators"
+  }
+}
+
 resource "kubernetes_manifest" "olm" {
-    depends_on = [ kubernetes_manifest.olm_crds ]
+    depends_on = [ kubernetes_manifest.olm_crds , kubernetes_namespace.olm, kubernetes_namespace.operators]
     for_each  = data.kubectl_file_documents.olm.manifests
     manifest = yamldecode(each.value)
+}
+
+resource "time_sleep" "olm_wait_destory" {
+  destroy_duration = "60s"
 }
