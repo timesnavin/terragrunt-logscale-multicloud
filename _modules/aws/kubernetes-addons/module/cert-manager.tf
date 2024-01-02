7421dcf1d233038621e1e126131074b7b2154dc9
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 12:45:43 -0500

    changes

--- _modules/aws/kubernetes-addons/module/cert-manager.tf
+++ _modules/aws/kubernetes-addons/module/cert-manager.tf
@@ -18,25 +18,25 @@ module "iam_eks_role_crtmgmr" {
 
 
 
-resource "helm_release" "cert-manager" {
-  namespace        = "cert-manager"
-  create_namespace = true
+# resource "helm_release" "cert-manager" {
+#   namespace        = "cert-manager"
+#   create_namespace = true
 
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "1.13.3"
+#   name       = "cert-manager"
+#   repository = "https://charts.jetstack.io"
+#   chart      = "cert-manager"
+#   version    = "1.13.3"
 
 
 
-  values = [
-    <<-EOT
-    installCRDs: true
-    serviceAccount:
-      name: cert-manager
-      annotations:
-        eks.amazonaws.com/role-arn: ${module.iam_eks_role_crtmgmr.iam_role_arn}
-    EOT
-  ]
-}
+#   values = [
+#     <<-EOT
+#     installCRDs: true
+#     serviceAccount:
+#       name: cert-manager
+#       annotations:
+#         eks.amazonaws.com/role-arn: ${module.iam_eks_role_crtmgmr.iam_role_arn}
+#     EOT
+#   ]
+# }
 
--- _modules/aws/kubernetes-addons/module/metricsserver.tf
+++ _modules/aws/kubernetes-addons/module/metricsserver.tf
@@ -1,19 +1,19 @@
 
-resource "helm_release" "metricsserver" {
-  namespace = "kube-system"
+# resource "helm_release" "metricsserver" {
+#   namespace = "kube-system"
 
-  name       = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server"
-  # repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  # repository_password = data.aws_ecrpublic_authorization_token.token.password
-  chart   = "metrics-server"
-  version = "v3.11.0"
+#   name       = "metrics-server"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server"
+#   # repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+#   # repository_password = data.aws_ecrpublic_authorization_token.token.password
+#   chart   = "metrics-server"
+#   version = "v3.11.0"
 
-  values = [
-    <<-EOT
-      replicas: 2
-      EOT
-  ]
+#   values = [
+#     <<-EOT
+#       replicas: 2
+#       EOT
+#   ]
 
 
-}
+# }
