2cb668b977a5c4e383f469a3d7847e947c94cdf3
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 11:40:22 -0500

    updates

--- _modules/aws/kubernetes-addons/module/olm.tf
+++ _modules/aws/kubernetes-addons/module/olm.tf
@@ -1,3 +1,8 @@
-resource "kubernetes_manifest" "container-test" {
-  manifest = yamldecode(file("manifests/olm/crds.yaml"))
+data "kubectl_file_documents" "olm_crds" {
+    content = file("manifests/olm/crds.yaml")
+}
+
+resource "kubernetes_manifest" "olm_crds" {
+    for_each  = data.kubectl_file_documents.docs.manifests
+    manifest = yamldecode(each.value)
 }
--- _modules/aws/kubernetes-addons/module/versions.tf
+++ _modules/aws/kubernetes-addons/module/versions.tf
@@ -14,6 +14,11 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.24.0"
     }
+      
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.7.0"
+    }
 
     null = {
       source  = "hashicorp/null"
