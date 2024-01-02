4e8eac1dd9d6dcdb5302fce2daab4b2f8dee0d52
Ryan Faircloth <ryan@dss-i.com>
2024-01-02 10:40:26 -0500

    Update karpenter.tf

--- _modules/aws/kubernetes-addons/module/karpenter.tf
+++ _modules/aws/kubernetes-addons/module/karpenter.tf
@@ -48,138 +48,139 @@ resource "random_string" "seed" {
   }
 
 }
-resource "kubernetes_manifest" "karpenter_node_class" {
-  manifest = yamldecode(<<-YAML
-    apiVersion: karpenter.k8s.aws/v1beta1
-    kind: EC2NodeClass
-    metadata:
-      name: default-${random_string.seed.result}
-    spec:
-      amiFamily: AL2
-      role: ${var.karpenter_role_name}
-      subnetSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${var.eks_cluster_name}
-      securityGroupSelectorTerms:
-        - tags:
-            karpenter.sh/discovery: ${var.eks_cluster_name}
-      tags:
-        karpenter.sh/discovery: ${var.eks_cluster_name}
-  YAML
-  )
 
-  depends_on = [
-    helm_release.karpenter
-  ]
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+# resource "kubernetes_manifest" "karpenter_node_class" {
+#   manifest = yamldecode(<<-YAML
+#     apiVersion: karpenter.k8s.aws/v1beta1
+#     kind: EC2NodeClass
+#     metadata:
+#       name: default-${random_string.seed.result}
+#     spec:
+#       amiFamily: AL2
+#       role: ${var.karpenter_role_name}
+#       subnetSelectorTerms:
+#         - tags:
+#             karpenter.sh/discovery: ${var.eks_cluster_name}
+#       securityGroupSelectorTerms:
+#         - tags:
+#             karpenter.sh/discovery: ${var.eks_cluster_name}
+#       tags:
+#         karpenter.sh/discovery: ${var.eks_cluster_name}
+#   YAML
+#   )
 
-resource "kubernetes_manifest" "karpenter_default_arm_node_pool" {
-  manifest = yamldecode(<<-YAML
-    apiVersion: karpenter.sh/v1beta1
-    kind: NodePool
-    metadata:
-      name: default-arm64
-    spec:
-      template:
-        metadata:
-          # Labels are arbitrary key-values that are applied to all nodes
-          labels:
-            computeType: "compute"
-            storageType: "network"
-            durabilityType: "provisioned"
-        spec:
-          nodeClassRef:
-            name: default-${random_string.seed.result}
-          requirements:
-            - key: karpenter.sh/capacity-type
-              operator: NotIn
-              values: ["spot"]          
-            - key: "karpenter.k8s.aws/instance-category"
-              operator: In
-              values: ["c", "m", "r"]
-            - key: "karpenter.k8s.aws/instance-cpu"
-              operator: In
-              values: ["4", "8", "16", "32"]
-            - key: "karpenter.k8s.aws/instance-hypervisor"
-              operator: In
-              values: ["nitro"]
-            - key: "kubernetes.io/arch"
-              operator: In
-              values: ["arm64"]              
-            - key: "karpenter.k8s.aws/instance-generation"
-              operator: Gt
-              values: ["2"]
-      limits:
-        cpu: 1000
-      disruption:
-        consolidationPolicy: WhenUnderutilized
-  YAML
-  )
+#   depends_on = [
+#     helm_release.karpenter
+#   ]
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-  depends_on = [
-    kubernetes_manifest.karpenter_node_class
-  ]
-  lifecycle {
-    replace_triggered_by = [
-      kubernetes_manifest.karpenter_node_class
-    ]
-    create_before_destroy = true
-  }
-}
+# resource "kubernetes_manifest" "karpenter_default_arm_node_pool" {
+#   manifest = yamldecode(<<-YAML
+#     apiVersion: karpenter.sh/v1beta1
+#     kind: NodePool
+#     metadata:
+#       name: default-arm64
+#     spec:
+#       template:
+#         metadata:
+#           # Labels are arbitrary key-values that are applied to all nodes
+#           labels:
+#             computeType: "compute"
+#             storageType: "network"
+#             durabilityType: "provisioned"
+#         spec:
+#           nodeClassRef:
+#             name: default-${random_string.seed.result}
+#           requirements:
+#             - key: karpenter.sh/capacity-type
+#               operator: NotIn
+#               values: ["spot"]          
+#             - key: "karpenter.k8s.aws/instance-category"
+#               operator: In
+#               values: ["c", "m", "r"]
+#             - key: "karpenter.k8s.aws/instance-cpu"
+#               operator: In
+#               values: ["4", "8", "16", "32"]
+#             - key: "karpenter.k8s.aws/instance-hypervisor"
+#               operator: In
+#               values: ["nitro"]
+#             - key: "kubernetes.io/arch"
+#               operator: In
+#               values: ["arm64"]              
+#             - key: "karpenter.k8s.aws/instance-generation"
+#               operator: Gt
+#               values: ["2"]
+#       limits:
+#         cpu: 1000
+#       disruption:
+#         consolidationPolicy: WhenUnderutilized
+#   YAML
+#   )
+
+#   depends_on = [
+#     kubernetes_manifest.karpenter_node_class
+#   ]
+#   lifecycle {
+#     replace_triggered_by = [
+#       kubernetes_manifest.karpenter_node_class
+#     ]
+#     create_before_destroy = true
+#   }
+# }
 
 
-resource "kubernetes_manifest" "karpenter_default_intel_node_pool" {
-  manifest = yamldecode(<<-YAML
-    apiVersion: karpenter.sh/v1beta1
-    kind: NodePool
-    metadata:
-      name: default-amd64
-    spec:
-      template:
-        metadata:
-          # Labels are arbitrary key-values that are applied to all nodes
-          labels:
-            computeType: "compute"
-            storageType: "network"
-            durabilityType: "provioned"
-        spec:
-          nodeClassRef:
-            name: default-${random_string.seed.result}
-          requirements:
-            - key: karpenter.sh/capacity-type
-              operator: NotIn
-              values: ["spot"]          
-            - key: "karpenter.k8s.aws/instance-category"
-              operator: In
-              values: ["c", "m", "r"]
-            - key: "karpenter.k8s.aws/instance-cpu"
-              operator: In
-              values: ["4", "8", "16", "32"]
-            - key: "karpenter.k8s.aws/instance-hypervisor"
-              operator: In
-              values: ["nitro"]
-            - key: "kubernetes.io/arch"
-              operator: In
-              values: ["amd64"]              
-            - key: "karpenter.k8s.aws/instance-generation"
-              operator: Gt
-              values: ["2"]
-      limits:
-        cpu: 1000
-      disruption:
-        consolidationPolicy: WhenUnderutilized
-  YAML
-  )
-  depends_on = [
-    kubernetes_manifest.karpenter_node_class
-  ]
-  lifecycle {
-    replace_triggered_by = [
-      kubernetes_manifest.karpenter_node_class
-    ]
-    create_before_destroy = true
-  }
-}
+# resource "kubernetes_manifest" "karpenter_default_intel_node_pool" {
+#   manifest = yamldecode(<<-YAML
+#     apiVersion: karpenter.sh/v1beta1
+#     kind: NodePool
+#     metadata:
+#       name: default-amd64
+#     spec:
+#       template:
+#         metadata:
+#           # Labels are arbitrary key-values that are applied to all nodes
+#           labels:
+#             computeType: "compute"
+#             storageType: "network"
+#             durabilityType: "provioned"
+#         spec:
+#           nodeClassRef:
+#             name: default-${random_string.seed.result}
+#           requirements:
+#             - key: karpenter.sh/capacity-type
+#               operator: NotIn
+#               values: ["spot"]          
+#             - key: "karpenter.k8s.aws/instance-category"
+#               operator: In
+#               values: ["c", "m", "r"]
+#             - key: "karpenter.k8s.aws/instance-cpu"
+#               operator: In
+#               values: ["4", "8", "16", "32"]
+#             - key: "karpenter.k8s.aws/instance-hypervisor"
+#               operator: In
+#               values: ["nitro"]
+#             - key: "kubernetes.io/arch"
+#               operator: In
+#               values: ["amd64"]              
+#             - key: "karpenter.k8s.aws/instance-generation"
+#               operator: Gt
+#               values: ["2"]
+#       limits:
+#         cpu: 1000
+#       disruption:
+#         consolidationPolicy: WhenUnderutilized
+#   YAML
+#   )
+#   depends_on = [
+#     kubernetes_manifest.karpenter_node_class
+#   ]
+#   lifecycle {
+#     replace_triggered_by = [
+#       kubernetes_manifest.karpenter_node_class
+#     ]
+#     create_before_destroy = true
+#   }
+# }
