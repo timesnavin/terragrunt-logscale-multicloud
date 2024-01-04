4630bbfe6da27c42cfb93529b3cd856031715c3d
Ryan Faircloth <ryan@dss-i.com>
2024-01-04 09:23:39 -0500

    Update karpenter.tf

--- _modules/aws/kubernetes-cluster/module/karpenter.tf
+++ _modules/aws/kubernetes-cluster/module/karpenter.tf
@@ -72,39 +72,39 @@ kind: NodePool
 metadata:
 name: compute-net-arm64
 spec:
-template:
-  metadata:
-    # Labels are arbitrary key-values that are applied to all nodes
-    labels:
-      computeType: "compute"
-      storageType: "network"
-      durabilityType: "provisioned"
-  spec:
-    nodeClassRef:
-      name: bottle-${random_string.seed.result}
-    requirements:
-      - key: karpenter.sh/capacity-type
-        operator: NotIn
-        values: ["spot"]          
-      - key: "karpenter.k8s.aws/instance-category"
-        operator: In
-        values: ["c", "m", "r"]
-      - key: "karpenter.k8s.aws/instance-cpu"
-        operator: In
-        values: ["4", "8", "16", "32"]
-      - key: "karpenter.k8s.aws/instance-hypervisor"
-        operator: In
-        values: ["nitro"]
-      - key: "kubernetes.io/arch"
-        operator: In
-        values: ["arm64"]              
-      - key: "karpenter.k8s.aws/instance-generation"
-        operator: Gt
-        values: ["2"]
-limits:
-  cpu: 1000
-disruption:
-  consolidationPolicy: WhenUnderutilized
+  template:
+    metadata:
+      # Labels are arbitrary key-values that are applied to all nodes
+      labels:
+        computeType: "compute"
+        storageType: "network"
+        durabilityType: "provisioned"
+    spec:
+      nodeClassRef:
+        name: bottle-${random_string.seed.result}
+      requirements:
+        - key: karpenter.sh/capacity-type
+          operator: NotIn
+          values: ["spot"]          
+        - key: "karpenter.k8s.aws/instance-category"
+          operator: In
+          values: ["c", "m", "r"]
+        - key: "karpenter.k8s.aws/instance-cpu"
+          operator: In
+          values: ["4", "8", "16", "32"]
+        - key: "karpenter.k8s.aws/instance-hypervisor"
+          operator: In
+          values: ["nitro"]
+        - key: "kubernetes.io/arch"
+          operator: In
+          values: ["arm64"]              
+        - key: "karpenter.k8s.aws/instance-generation"
+          operator: Gt
+          values: ["2"]
+  limits:
+    cpu: 1000
+  disruption:
+    consolidationPolicy: WhenUnderutilized
 YAML
 
   depends_on = [
