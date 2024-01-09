c8aef5ae208535c2947cf3038f530b1d35af43d7
Ryan Faircloth <ryan@dss-i.com>
2024-01-09 07:18:53 -0500

    updates

---
+++ _modules/aws/kubernetes-stacked/module/eks-addon-metrics-server.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "metrics-server" {
+  depends_on = [
+    time_sleep.karpenter_nodes
+  ]
+  namespace = "kube-system"
+
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "v1.6.2"
+
+  wait = true
+  values = [
+    file("./eks-addon-metrics-server.yaml")
+  ]
+
+
+}
