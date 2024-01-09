36a573f92d6f362db2dcc6992961f2f1bd8d2243
Ryan Faircloth <ryan@dss-i.com>
2024-01-09 06:09:41 -0500

    changes

--- _modules/aws/kubernetes-stacked/module/eks-addon-ing-alb.tf
+++ _modules/aws/kubernetes-stacked/module/eks-addon-ing-alb.tf
@@ -14,5 +14,36 @@ module "ing_alb_irsa" {
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
   }
+}
+
+
+
+data "kubectl_path_documents" "alb_controller_crds" {
+  pattern = "./manifests/alb-controller/v2.6.1.yaml"
+}
+
+resource "kubectl_manifest" "alb_controller_crds" {
+  for_each  = toset(data.kubectl_path_documents.alb_controller_crds.documents)
+  yaml_body = each.value
+}
+
+resource "helm_release" "alb-controller" {
+  depends_on = [
+    time_sleep.karpenter_nodes,
+    helm_release.karpenter,
+    kubectl_manifest.alb_controller_crds,
+    helm_release.cert-manager
+  ]
+  namespace        = "kube-system"
+  
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "alb-controller"
+  version    = "v1.6.1"
+
+  wait   = true
+  values = [templatefile("./eks-addon-ing-alb-values.yaml",{clusterName=var.cluster_name,irsaarn=module.ing_alb_irsa.iam_role_arn}})]
+
 
 }
+# 
