049b2058ccf4925a6fe93297f7949cd4dc104c3d
Ryan Faircloth <ryan@dss-i.com>
2024-01-09 11:42:33 -0500

    add ui

--- _modules/aws/kubernetes-stacked/module/k8s-zalando-operator.tf
+++ _modules/aws/kubernetes-stacked/module/k8s-zalando-operator.tf
@@ -14,3 +14,20 @@ resource "helm_release" "zalando-operator" {
 
   values = [templatefile("./k8s-zalando-operator.yaml",{region=var.cluster_region})]
 }
+
+resource "helm_release" "zalando-operator-ui" {
+    depends_on = [
+    time_sleep.karpenter_nodes,
+    helm_release.karpenter
+  ]
+
+  namespace        = "postgres-operator"
+  create_namespace = true
+
+  name       = "postgres-operator-ui"
+  repository = "https://opensource.zalando.com/postgres-operator/charts/postgres-operator"
+  chart      = "postgres-operator-ui"
+  version    = "1.10.1"
+
+  values = [templatefile("./k8s-zalando-operator.yaml",{region=var.cluster_region})]
+}
