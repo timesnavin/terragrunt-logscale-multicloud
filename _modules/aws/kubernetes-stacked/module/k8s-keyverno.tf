8c28d0b9871ba58c0a01c5990e7104b583dc22c6
Ryan Faircloth <ryan@dss-i.com>
2024-01-09 09:23:44 -0500

    keyverno

---
+++ _modules/aws/kubernetes-stacked/module/k8s-keyverno.tf
@@ -0,0 +1,39 @@
+
+resource "helm_release" "keyverno" {
+  depends_on = [
+    time_sleep.karpenter_nodes,
+    helm_release.karpenter
+  ]
+  namespace = "keyverno"
+  
+
+  name       = "keyverno"
+  repository = "https://kyverno.github.io/kyverno/"
+  chart      = "keyverno"
+  version    = "3.0.5"
+
+  wait = false
+
+  values = [file("./k8s-keyverno-values.yaml")]
+}
+
+
+
+resource "helm_release" "keyverno-policies" {
+  depends_on = [
+    time_sleep.karpenter_nodes,
+    helm_release.karpenter,
+    helm_release.keyverno
+  ]
+  namespace = "keyverno"
+  
+
+  name       = "keyverno"
+  repository = "https://kyverno.github.io/kyverno/"
+  chart      = "keyverno-policies"
+  version    = "3.0.4"
+
+  wait = false
+
+  values = [file("./k8s-keyverno-policies-values.yaml")]
+}
