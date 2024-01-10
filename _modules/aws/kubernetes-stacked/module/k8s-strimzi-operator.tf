23000af380d9ba4a592da810a949edfe526d67b7
Ryan Faircloth <ryan@dss-i.com>
2024-01-10 18:22:16 -0500

    fix

--- _modules/aws/kubernetes-stacked/module/k8s-strimzi-operator.tf
+++ _modules/aws/kubernetes-stacked/module/k8s-strimzi-operator.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "strimzi-operator" {
-    depends_on = [
+  depends_on = [
     time_sleep.karpenter_nodes,
     helm_release.karpenter
   ]
@@ -9,8 +9,26 @@ resource "helm_release" "strimzi-operator" {
 
   name       = "strimzi-operator"
   repository = "https://strimzi.io/charts/"
-  chart      = "strimzi-operator"
-  version    = "0.20.1"
+  chart      = "strimzi-kafka-operator"
+  version    = "0.39.0"
 
-  values = [templatefile("./k8s-strimzi-operator.yaml",{})]
+  values = [templatefile("./k8s-strimzi-operator.yaml", {})]
+}
+
+resource "helm_release" "strimzi-dc" {
+  depends_on = [
+    time_sleep.karpenter_nodes,
+    helm_release.karpenter,
+    helm_release.strimzi-operator
+  ]
+
+  namespace        = "strimzi-operator"
+  create_namespace = true
+
+  name       = "strimzi-dc"
+  repository = "https://strimzi.io/charts/"
+  chart      = "strimzi-drain-cleaner"
+  version    = "1.0.1"
+
+  values = [templatefile("./k8s-strimzi-drain-cleaner.yaml", {})]
 }
