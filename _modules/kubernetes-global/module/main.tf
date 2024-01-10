f0aa771d0a82b25158c887f4da7e0bcd9468cd8b
Ryan Faircloth <ryan@dss-i.com>
2024-01-10 06:47:28 -0500

    Update main.tf

--- _modules/kubernetes-global/module/main.tf
+++ _modules/kubernetes-global/module/main.tf
@@ -7,35 +7,35 @@ resource "kubernetes_namespace" "sso" {
   }
 }
 
-# resource "kubectl_manifest" "test" {
-#     yaml_body = <<YAML
-# kind: "postgresql"
-# apiVersion: "acid.zalan.do/v1"
+resource "kubectl_manifest" "ssodb" {
+    yaml_body = <<YAML
+kind: "postgresql"
+apiVersion: "acid.zalan.do/v1"
 
-# metadata:
-#   name: "db"
-#   namespace: "${kubernetes_namespace.sso.metadata.0.name}}"
-#   labels:
-#     team: acid
+metadata:
+  name: "db"
+  namespace: "${kubernetes_namespace.sso.metadata.0.name}}"
+  labels:
+    team: acid
 
-# spec:
-#   teamId: "acid"
-#   postgresql:
-#     version: "15"
-#   numberOfInstances: 1
-#   volume:
-#     size: "10Gi"
-#   users:
-#     kk: []
-#   databases:
-#     keycloak: kk
+spec:
+  teamId: "acid"
+  postgresql:
+    version: "15"
+  numberOfInstances: 1
+  volume:
+    size: "10Gi"
+  users:
+    kk: []
+  databases:
+    keycloak: kk
   
-#   resources:
-#     requests:
-#       cpu: 100m
-#       memory: 100Mi
-#     limits:
-#       cpu: 500m
-#       memory: 500Mi
-# YAML
-# }
+  resources:
+    requests:
+      cpu: 100m
+      memory: 100Mi
+    limits:
+      cpu: 500m
+      memory: 500Mi
+YAML
+}
