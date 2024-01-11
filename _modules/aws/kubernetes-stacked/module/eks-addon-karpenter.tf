e7b1945ea51e8c4d7d2204578df10ca0223578c1
Ryan Faircloth <ryan@dss-i.com>
2024-01-11 11:15:33 -0500

    Update eks-addon-karpenter.tf

--- _modules/aws/kubernetes-stacked/module/eks-addon-karpenter.tf
+++ _modules/aws/kubernetes-stacked/module/eks-addon-karpenter.tf
@@ -7,7 +7,7 @@ module "karpenter" {
 
   iam_role_use_name_prefix = true
   irsa_path = var.iam_role_path
-  rule_name_prefix = var.iam_policy_name_prefix
+  # rule_name_prefix = "ll"
   
   # In v0.32.0/v1beta1, Karpenter now creates the IAM instance profile
   # so we disable the Terraform creation and add the necessary permissions for Karpenter IRSA
