509edbbdc001c227a9e7858579ef77fbf8e4c6ff
Ryan Faircloth <ryan@dss-i.com>
2024-01-11 09:21:04 -0500

    Update eks.tf

--- _modules/aws/kubernetes-base/module/eks.tf
+++ _modules/aws/kubernetes-base/module/eks.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "cluster" {
-  name               = "cluster"
+  name_prefix =                var.cluster_name
   path               = var.iam_role_path
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
