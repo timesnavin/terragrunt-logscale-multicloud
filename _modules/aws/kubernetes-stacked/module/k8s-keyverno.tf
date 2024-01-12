22ee7d55c6609ec97200640b6e9ca178bcac41c3
Ryan Faircloth <ryan@dss-i.com>
2024-01-12 09:09:39 -0500

    Update k8s-keyverno.tf

--- _modules/aws/kubernetes-stacked/module/k8s-keyverno.tf
+++ _modules/aws/kubernetes-stacked/module/k8s-keyverno.tf
@@ -1,52 +1,52 @@
 
-resource "helm_release" "kyverno" {
-  depends_on = [
-    time_sleep.karpenter_nodes,
-    helm_release.karpenter,
-    #helm_release.cert-manager
-  ]
-  namespace        = "kyverno"
-  create_namespace = true
+# resource "helm_release" "kyverno" {
+#   depends_on = [
+#     time_sleep.karpenter_nodes,
+#     helm_release.karpenter,
+#     #helm_release.cert-manager
+#   ]
+#   namespace        = "kyverno"
+#   create_namespace = true
 
 
-  name       = "kyverno"
-  repository = "https://kyverno.github.io/kyverno/"
-  chart      = "kyverno"
-  version    = "3.1.3"
+#   name       = "kyverno"
+#   repository = "https://kyverno.github.io/kyverno/"
+#   chart      = "kyverno"
+#   version    = "3.1.3"
 
-  wait = false
+#   wait = false
 
-  values = [file("./k8s-kyverno-values.yaml")]
-}
-
-
-
-resource "helm_release" "kyverno-policies" {
-  depends_on = [
-    time_sleep.karpenter_nodes,
-    helm_release.karpenter,
-    helm_release.kyverno
-  ]
-  namespace = "kyverno"
+#   values = [file("./k8s-kyverno-values.yaml")]
+# }
 
 
-  name       = "kyverno-policies"
-  repository = "https://kyverno.github.io/kyverno/"
-  chart      = "kyverno-policies"
-  version    = "3.1.3"
 
-  wait = false
+# resource "helm_release" "kyverno-policies" {
+#   depends_on = [
+#     time_sleep.karpenter_nodes,
+#     helm_release.karpenter,
+#     helm_release.kyverno
+#   ]
+#   namespace = "kyverno"
 
-  values = [file("./k8s-kyverno-policies-values.yaml")]
-}
+
+#   name       = "kyverno-policies"
+#   repository = "https://kyverno.github.io/kyverno/"
+#   chart      = "kyverno-policies"
+#   version    = "3.1.3"
+
+#   wait = false
+
+#   values = [file("./k8s-kyverno-policies-values.yaml")]
+# }
 
 
-data "kubectl_path_documents" "kyverno-policies" {
-  pattern = "./manifests/kyverno-policies/*.yaml"
-}
+# data "kubectl_path_documents" "kyverno-policies" {
+#   pattern = "./manifests/kyverno-policies/*.yaml"
+# }
 
-resource "kubectl_manifest" "kyverno-policies" {
-  depends_on = [helm_release.kyverno]
-  count      = length(data.kubectl_path_documents.kyverno-policies.documents)
-  yaml_body  = element(data.kubectl_path_documents.kyverno-policies.documents, count.index)
-}
+# resource "kubectl_manifest" "kyverno-policies" {
+#   depends_on = [helm_release.kyverno]
+#   count      = length(data.kubectl_path_documents.kyverno-policies.documents)
+#   yaml_body  = element(data.kubectl_path_documents.kyverno-policies.documents, count.index)
+# }
