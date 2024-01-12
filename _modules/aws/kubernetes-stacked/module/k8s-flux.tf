2ad47130c845f34e507e6ed48569884f7b724337
Ryan Faircloth <ryan@dss-i.com>
2024-01-12 04:52:19 -0500

    use argocd

---
+++ _modules/aws/kubernetes-stacked/module/k8s-flux.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "flux2" {
+  repository       = "https://fluxcd-community.github.io/helm-charts"
+  chart            = "flux2"
+  name             = "flux2"
+  namespace        = "flux-system"
+  create_namespace = true
+}
--- _modules/aws/kubernetes-stacked/module/main.tf
+++ _modules/aws/kubernetes-stacked/module/main.tf
@@ -55,26 +55,6 @@ provider "kubectl" {
 
 
 
-provider "argocd" {
-  # Configuration options
-  server_addr = "http://127.0.0.1:9090"
-  auth_token =""
-  port-forward = true
-  port-forward-namespace = kubernetes_namespace.argocd.metadata.0.name
-
-    kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
-    }
-}
-
-
 
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
