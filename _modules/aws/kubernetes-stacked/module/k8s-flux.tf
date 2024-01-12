e9733240a97f6af4844d80a3997b3b06a5d63560
Ryan Faircloth <ryan@dss-i.com>
2024-01-12 12:43:13 -0500

    updates

--- _modules/aws/kubernetes-stacked/module/k8s-flux.tf
+++ _modules/aws/kubernetes-stacked/module/k8s-flux.tf
@@ -5,6 +5,10 @@ resource "helm_release" "flux2" {
   namespace        = "flux-system"
   create_namespace = true
   version          = "2.12.2"
+  values = [<<YAML
+  logLevel: debug
+  YAML
+  ]
 }
 
 
