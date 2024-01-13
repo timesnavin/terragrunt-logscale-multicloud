dd9b57181af13eb48917bd4f559257686940a079
Ryan Faircloth <ryan@dss-i.com>
2024-01-13 08:20:46 -0500

    kafka deployed

--- _modules/aws/logscale/module/main.tf
+++ _modules/aws/logscale/module/main.tf
@@ -1,1 +1,28 @@
 data "aws_caller_identity" "current" {}
+
+data "kubectl_path_documents" "flux2-repos" {
+  pattern = "./manifests/flux-repos/*.yaml"
+}
+
+resource "kubectl_manifest" "flux2-repos" {
+  depends_on = [
+    kubernetes_namespace.logscale
+  ]
+  override_namespace = var.namespace
+  for_each           = data.kubectl_path_documents.flux2-repos.manifests
+  yaml_body          = each.value
+}
+
+
+data "kubectl_path_documents" "flux2-releases" {
+  pattern = "./manifests/flux-releases/*.yaml"
+}
+
+resource "kubectl_manifest" "flux2-releases" {
+  depends_on = [
+    kubectl_manifest.flux2-repos
+  ]
+  override_namespace = var.namespace
+  for_each           = data.kubectl_path_documents.flux2-releases.manifests
+  yaml_body          = each.value
+}
