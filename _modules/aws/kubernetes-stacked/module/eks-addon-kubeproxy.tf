86914c59684098f438d18ff283334fc852844a02
Ryan Faircloth <ryan@dss-i.com>
2024-01-16 17:50:35 -0500

    upgrade to 1.28

--- _modules/aws/kubernetes-stacked/module/eks-addon-kubeproxy.tf
+++ _modules/aws/kubernetes-stacked/module/eks-addon-kubeproxy.tf
@@ -2,7 +2,7 @@
 resource "aws_eks_addon" "kube-proxy" {
   cluster_name                = var.cluster_name
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.28.4-eksbuild.1"
+  addon_version               = "v1.28.4-eksbuild.4"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
   preserve                    = true
