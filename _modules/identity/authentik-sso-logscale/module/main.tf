83bfd671b6e5f49b22ce0d141f48865df085d36d
Ryan Faircloth <ryan@dss-i.com>
2024-01-22 09:07:34 -0500

    Update main.tf

--- _modules/identity/authentik-sso-logscale/module/main.tf
+++ _modules/identity/authentik-sso-logscale/module/main.tf
@@ -27,7 +27,7 @@ resource "random_uuid" "slug" {
 
 resource "authentik_application" "name" {
   name = var.app_name
-  slug = resource.random_uuid.slug
+  slug = resource.random_uuid.slug.result
 
   protocol_provider = authentik_provider_saml.this
 }
