b86e1b68ccf523dd1804824d80b16c8295c78e79
Ryan Faircloth <ryan@dss-i.com>
2024-01-31 11:49:57 -0500

    fix: correct zone math

--- _modules/aws/azs/module/outputs.tf
+++ _modules/aws/azs/module/outputs.tf
@@ -9,8 +9,8 @@ locals {
   public_prefixes  = slice(["0","1","2","3","4","5","6","7","8","9"],0,local.azCount)
   private_prefixes  = slice(["10","11","12","13","14","15","16","17","18","19"],local.azCount, local.azCount * 2)
   
-  public_subnets  = slice(cidrsubnets(local.subnets[0], "4", "4", "4", "4", "4", "4", "4"), 0, var.max_zones)
-  private_subnets = slice(cidrsubnets(local.subnets[1], "4", "4", "4", "4", "4", "4", "4"), 0, var.max_zones)
+  public_subnets  = slice(cidrsubnets(local.subnets[0], "4", "4", "4", "4", "4", "4", "4"), 0, local.azCount)
+  private_subnets = slice(cidrsubnets(local.subnets[1], "4", "4", "4", "4", "4", "4", "4"), 0, local.azCount)
 
 }
 output "public_subnet_ipv6_prefixes" {
