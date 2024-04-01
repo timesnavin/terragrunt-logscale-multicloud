7753c6e28921bbbb6ee68b0493f72ec76e281d0a
Ryan Faircloth <ryan@dss-i.com>
2024-04-01 09:12:35 -0400

    fix: allow any repo within tenant to assume role

--- _modules/aws/logscale/module/aws-ingest-assume-role.tf
+++ _modules/aws/logscale/module/aws-ingest-assume-role.tf
@@ -22,18 +22,39 @@ module "ingest-role-actor" {
   role_name_prefix  = "${local.namespace}_${var.service_account}_actor"
   role_requires_mfa = false
 
-  trusted_role_arns = [
-    module.ingest-role.iam_role_arn,
-  ]
-  role_sts_externalid = "SINGLE_ORGANIZATION_ID/pSGN5GYDENjCDzpuVL5dTnrP"
+  # trusted_role_arns = [
+  #   module.ingest-role.iam_role_arn,
+  # ]
+  # role_sts_externalid = "SINGLE_ORGANIZATION_ID/*"
 
   custom_role_policy_arns = [
     module.iam_iam-policy-s3log.arn
   ]
 
+  create_custom_role_trust_policy = true
+  custom_role_trust_policy        = data.aws_iam_policy_document.custom_trust_policy.json
+
 }
 
 
+data "aws_iam_policy_document" "custom_trust_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    condition {
+      test     = "StringLike"
+      variable = "sts:ExternalId"
+      values   = ["SINGLE_ORGANIZATION_ID/*"]
+    }
+
+
+    principals {
+      type        = "AWS"
+      identifiers = [module.ingest-role.iam_role_arn]
+    }
+  }
+}
 module "iam_iam-assume_ingest-actor" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "5.37.2"
